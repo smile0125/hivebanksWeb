33c2e49e1b2fe9b719b7c7cb518d44c42625a6ac
edwin <1045416962@qq.com>
2018-08-30 19:05:09 +0800

    hivebanksWeb

---
+++ assets/js/common.js
@@ -0,0 +1,124 @@
+// 设置cookies函数
+function SetCookie(name, value) {
+    var now = new Date();
+    var time = now.getTime();
+    // 有效期7天
+    time += 3600 * 1000 * 24 * 7;
+    now.setTime(time);
+    document.cookie = name + "=" + escape(value) + '; expires=' + now.toUTCString();
+}
+
+// 取cookies函数
+function GetCookie(name) {
+    var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+    if (arr != null) return unescape(arr[2]);
+    return null;
+}
+
+// 删除cookie函数
+function DelCookie(name) {
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval = GetCookie(name);
+    if (cval != null) document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+}
+
+// 取得URL参数
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    console.log(r);
+    if (r != null) return unescape(r[2]);
+    return null;
+    // console.log(r);
+}
+
+// Email格式检查
+function IsEmail(s) {
+    var patrn = /^(?:\w+\.?)*\w+@(?:\w+\.)*\w+$/;
+    return patrn.test(s);
+}
+
+// 调用API共通函数
+function CallApi(api_url, post_data, suc_func, error_func) {
+
+    var api_site = 'http://www.fnying.com/php/';
+
+    post_data = post_data || {};
+    suc_func = suc_func || function () {};
+    error_func = error_func || function () {};
+
+    //console.log('Call API:' + api_url);
+    //console.log(JSON.stringify(post_data));
+
+    $.ajax({
+        url: api_site + api_url,
+        dataType: "jsonp",
+        data: post_data,
+        success: function (response) {
+            //console.log(JSON.stringify(response));
+            // API返回失败
+            if (response.errcode != 0) {
+                error_func(response);
+            } else {
+                // 成功处理数据
+                suc_func(response);
+            }
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            // API错误异常
+            var response = {"errcode": -1, "errmsg": '系统异常，请稍候再试'};
+            // 异常处理
+            error_func(response);
+        }
+    });
+}
+// 获取用户UUID
+function GetUUID(post_data, suc_func, error_func) {
+    var api_url = 'get_uuid.php';
+    CallApi(api_url, post_data, suc_func, error_func);
+}
+
+//contact us
+function ContactUs(uuid,user_name, user_email, user_suggestion,suc_func, error_func){
+    var api_url = 'contact_us.php';
+    var post_data = {"uuid": uuid,"user_name": user_name, "user_email": user_email, "user_suggestion": user_suggestion};
+    CallApi(api_url, post_data, suc_func, error_func);
+}
+
+/**
+ * 禁用按钮
+ * @param $this 按钮对象
+ * @param btnText 按钮文本内容 默认为"处理中"
+ * @return {boolean}
+ */
+function DisableClick($this, btnText) {
+    if (!$this) {
+        console.warn("$this 不能为空");
+        return true;
+    }
+    var status = Number($this.attr('data-clickStatus') || 1);
+    if (status == 0) {
+        return true;
+    }
+
+    btnText = btnText ? btnText : "处理中";
+    $this.attr('data-clickStatus', 0);
+    $this.html(btnText);
+    return false;
+}
+
+/**
+ * 激活按钮
+ * @param $this 按钮对象
+ * @param btnText 按钮文本内容 默认为"处理中"
+ */
+function ActiveClick($this, btnText) {
+    if (!$this) {
+        console.warn("$this 不能为空");
+        return;
+    }
+    btnText = btnText ? btnText : "确认";
+    $this.attr('data-clickStatus', 1);
+    $this.html(btnText);
+}
---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+$(function(){
+    //获取uuid
+    var uuid = GetCookie('UUID');
+    console.log("已存在UUID"+uuid);
+    if (!uuid) {
+        uuid = new Date().getTime();
+        console.log("不存在UUID"+uuid);
+        // 取得UUID
+        GetUUID('', function (response) {
+            if (response.errcode == '0') {
+                uuid = response.uuid;
+                SetCookie('UUID', uuid);
+            }
+        }, function (response) {});
+    }
+
+    //contact us
+    $(".form>.button").click(function(){
+        var firstname=$("input[name='firstname']").val();
+        var lastname=$("input[name='lastname']").val();
+        var user_name=lastname+firstname;
+        var user_email=$("input[name='email']").val();
+        var user_suggestion=$("textarea[name='message']").val();
+        if(firstname.length<=0){layer.msg("First name cannot be empty.")}
+        if(lastname.length<=0){layer.msg("Last name cannot be empty.")}
+        if(user_email.length<=0){layer.msg("Mailbox cannot be empty.")}
+        if(!(IsEmail(user_email))){layer.msg("Email format error")}
+        if(user_suggestion.length<=0){layer.msg("Message cannot be empty.")}
+
+        var $this = $(this);
+        if (DisableClick($this)) return;
+        ContactUs(uuid,user_name, user_email, user_suggestion, function (response) {
+            ActiveClick($this, 'Submit');
+            if (response.errcode == '0') {
+                $("input[name='firstname']").val("");
+                $("input[name='lastname']").val("");
+                $("input[name='email']").val("");
+                $("textarea[name='message']").val("");
+            }
+            layer.msg(response.errmsg);
+        }, function (response) {
+            ActiveClick($this, 'Submit');
+            layer.msg(response.errmsg);
+        });
+    })
+});
